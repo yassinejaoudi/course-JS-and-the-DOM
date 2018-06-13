36c3aad3ce827fa1e5ef9a86ca083e83962c18df
yassinejaoudi <jaoudiyassine@gmail.com>
2018-06-13 19:42:22 -0400

    Add event listener to understand the DOM

---
+++ app.js
@@ -0,0 +1,10 @@
+const subHeader = document.querySelector('.text-center');
+const partners = document.querySelector('.partners__featured');
+
+subHeader.addEventListener('click', ()=>{
+    console.log('You clicked and I listened!!');
+});
+
+partners.addEventListener('mouseover', ()=>{
+    console.log('this is our parteners for this course');
+});
